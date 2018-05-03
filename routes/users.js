13f69b41375f5a30df22318e10453a5220738abc
silif <1054927886@qq.com>
2018-05-03 19:50:25 +0800

    userlist

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {
+    name:'yangdong',
+    age:26,
+    id:1
+  },{
+    name:'jingjing',
+    age:25,
+    id:2
+  },{
+    name:'jian yang',
+    age:25,
+    id:3
+  }
+]
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({users:users});
 });
 
 module.exports = router;
